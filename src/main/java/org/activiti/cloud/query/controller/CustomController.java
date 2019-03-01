134ef2a05799d84de47ec28eb4b81133151097cd
Lukas <slamka.lukas@gmail.com>
2019-03-01 22:57:00 +0100

    task rest

--- src/main/java/org/activiti/cloud/query/controller/CustomController.java
+++ src/main/java/org/activiti/cloud/query/controller/CustomController.java
@@ -1,14 +1,143 @@
 package org.activiti.cloud.query.controller;
 
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import org.activiti.api.runtime.shared.security.SecurityManager;
+import org.activiti.cloud.alfresco.data.domain.AlfrescoPagedResourcesAssembler;
+import org.activiti.cloud.services.query.app.repository.EntityFinder;
+import org.activiti.cloud.services.query.app.repository.TaskRepository;
+import org.activiti.cloud.services.query.app.repository.TaskVariableRepository;
+import org.activiti.cloud.services.query.model.QTaskEntity;
+import org.activiti.cloud.services.query.model.QTaskVariableEntity;
+import org.activiti.cloud.services.query.model.TaskEntity;
+import org.activiti.cloud.services.query.model.TaskVariableEntity;
+import org.activiti.cloud.services.query.resources.TaskResource;
+import org.activiti.cloud.services.query.resources.VariableResource;
+import org.activiti.cloud.services.query.rest.TaskController;
+import org.activiti.cloud.services.query.rest.assembler.TaskResourceAssembler;
+import org.activiti.cloud.services.query.rest.assembler.TaskVariableResourceAssembler;
+import org.activiti.cloud.services.security.ActivitiForbiddenException;
+import org.activiti.cloud.services.security.TaskLookupRestrictionService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.querydsl.binding.QuerydslPredicate;
+import org.springframework.hateoas.PagedResources;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(
+        value = "/abpm"
+)
 public class CustomController {
 
-    @RequestMapping("/abpm")
+    private TaskRepository taskRepository;
+
+    private TaskVariableRepository variableRepository;
+
+    private TaskResourceAssembler taskResourceAssembler;
+
+    private EntityFinder entityFinder;
+
+    private TaskLookupRestrictionService taskLookupRestrictionService;
+
+    private SecurityManager securityManager;
+
+    private TaskVariableResourceAssembler variableResourceAssembler;
+
+    private AlfrescoPagedResourcesAssembler<TaskVariableEntity> pagedResourcesAssembler;
+
+    private AlfrescoPagedResourcesAssembler<TaskEntity> pagedTaskResourcesAssembler;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TaskController.class);
+
+    @Autowired
+    public CustomController(TaskRepository taskRepository,
+                          TaskResourceAssembler taskResourceAssembler,
+                          EntityFinder entityFinder,
+                          TaskLookupRestrictionService taskLookupRestrictionService,
+                          SecurityManager securityManager,
+                          TaskVariableRepository variableRepository,
+                          TaskVariableResourceAssembler variableResourceAssembler,
+                          AlfrescoPagedResourcesAssembler<TaskVariableEntity> pagedResourcesAssembler,
+                          AlfrescoPagedResourcesAssembler<TaskEntity> pagedTaskResourcesAssembler){
+        this.taskRepository = taskRepository;
+        this.taskResourceAssembler = taskResourceAssembler;
+        this.entityFinder = entityFinder;
+        this.taskLookupRestrictionService = taskLookupRestrictionService;
+        this.securityManager = securityManager;
+        this.variableRepository = variableRepository;
+        this.variableResourceAssembler = variableResourceAssembler;
+        this.pagedResourcesAssembler = pagedResourcesAssembler;
+        this.pagedTaskResourcesAssembler = pagedTaskResourcesAssembler;
+    }
+
+    @RequestMapping("/test")
     public String index() {
         return "Greetings from Spring Boot!";
+    }
+
+    @RequestMapping(value = "/{taskId}", method = RequestMethod.GET)
+    public TaskResource findById(@PathVariable String taskId) {
+
+        TaskEntity taskEntity = entityFinder.findById(taskRepository,
+                taskId,
+                "Unable to find taskEntity for the given id:'" + taskId + "'");
+
+        //do restricted query and check if still able to see it
+        Iterable<TaskEntity> taskIterable = taskRepository.findAll(taskLookupRestrictionService.restrictTaskQuery(QTaskEntity.taskEntity.id.eq(taskId)));
+        if (!taskIterable.iterator().hasNext()) {
+            LOGGER.debug("User " + securityManager.getAuthenticatedUserId() + " not permitted to access taskEntity " + taskId);
+            throw new ActivitiForbiddenException("Operation not permitted for " + taskId);
+        }
+        return taskResourceAssembler.toResource(taskEntity);
+    }
+
+    @RequestMapping(value = "/{taskId}/new", method = RequestMethod.GET)
+    public PagedResources<TaskResource> getVariables1(@PathVariable String taskId,
+                                                         @QuerydslPredicate(root = TaskEntity.class) Predicate predicate,
+                                                         Pageable pageable) {
+
+        QTaskEntity task = QTaskEntity.taskEntity;
+        BooleanExpression expression = task.id.eq(taskId);
+
+        Predicate extendedPredicated = expression;
+        if (predicate != null) {
+            extendedPredicated = expression.and(predicate);
+        }
+
+        Page<TaskEntity> tasks = taskRepository.findAll(extendedPredicated,
+                pageable);
+
+        return pagedTaskResourcesAssembler.toResource(pageable,
+                tasks,
+                taskResourceAssembler);
+    }
+
+    @RequestMapping(value = "/{taskId}/variables", method = RequestMethod.GET)
+    public PagedResources<VariableResource> getVariables2(@PathVariable String taskId,
+                                                         @QuerydslPredicate(root = TaskVariableEntity.class) Predicate predicate,
+                                                         Pageable pageable) {
+
+        QTaskVariableEntity variable = QTaskVariableEntity.taskVariableEntity;
+        BooleanExpression expression = variable.taskId.eq(taskId);
+
+        Predicate extendedPredicated = expression;
+        if (predicate != null) {
+            extendedPredicated = expression.and(predicate);
+        }
+
+        Page<TaskVariableEntity> variables = variableRepository.findAll(extendedPredicated,
+                pageable);
+
+        return pagedResourcesAssembler.toResource(pageable,
+                variables,
+                variableResourceAssembler);
     }
 
 }
--- src/test/java/org/activiti/cloud/query/controller/CustomControllerTest.java
+++ src/test/java/org/activiti/cloud/query/controller/CustomControllerTest.java
@@ -1,33 +1,191 @@
 package org.activiti.cloud.query.controller;
 
+import com.querydsl.core.types.Predicate;
+import org.activiti.api.runtime.shared.security.SecurityManager;
+import org.activiti.api.task.model.Task;
+import org.activiti.cloud.services.query.app.repository.EntityFinder;
+import org.activiti.cloud.services.query.app.repository.ProcessDefinitionRepository;
+import org.activiti.cloud.services.query.app.repository.TaskRepository;
+import org.activiti.cloud.services.query.app.repository.TaskVariableRepository;
+import org.activiti.cloud.services.query.model.TaskEntity;
+import org.activiti.cloud.services.query.model.TaskVariableEntity;
+import org.activiti.cloud.services.security.TaskLookupRestrictionService;
+import org.activiti.core.common.spring.security.policies.SecurityPoliciesManager;
+import org.activiti.core.common.spring.security.policies.conf.SecurityPoliciesProperties;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
+import java.util.*;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @AutoConfigureMockMvc(secure = false)
 @SpringBootTest
 public class CustomControllerTest {
 
+    private static final String TASK_ALFRESCO_IDENTIFIER = "task-alfresco";
+    private static final String TASK_IDENTIFIER = "task";
+
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private TaskRepository taskRepository;
+
+    @MockBean
+    private EntityFinder entityFinder;
+
+    @MockBean
+    private TaskLookupRestrictionService taskLookupRestrictionService;
+
+    @MockBean
+    private SecurityManager securityManager;
+
+    @MockBean
+    private SecurityPoliciesManager securityPoliciesManager;
+
+    @MockBean
+    private ProcessDefinitionRepository processDefinitionRepository;
+
+    @MockBean
+    private SecurityPoliciesProperties securityPoliciesProperties;
+
+    @MockBean
+    private TaskVariableRepository variableRepository;
 
     @Test
     public void testABPMNendpoint() throws Exception {
 
         //when
-        MvcResult result = mockMvc.perform(get("/abpm"))
+        MvcResult result = mockMvc.perform(get("/abpm/test"))
                 .andReturn();
 
         assertThat(result.getResponse().getContentAsString()).isEqualTo("Greetings from Spring Boot!");
     }
 
+    @Test
+    public void taskFinderTest() throws Exception {
+        //given
+        TaskEntity taskEntity = buildDefaultTask();
+        given(entityFinder.findById(eq(taskRepository),
+                eq(taskEntity.getId()),
+                anyString()))
+                .willReturn(taskEntity);
+
+        Predicate restrictionPredicate = mock(Predicate.class);
+        given(taskLookupRestrictionService.restrictTaskQuery(any())).willReturn(restrictionPredicate);
+        given(taskRepository.findAll(restrictionPredicate)).willReturn(Collections.singletonList(taskEntity));
+
+        //when
+        MvcResult mvcResult = mockMvc.perform(get("/abpm/{taskId}",
+                taskEntity.getId()).accept(MediaType.APPLICATION_JSON_VALUE))
+                //then
+                .andExpect(status().isOk())
+                .andReturn();
+
+        assertThat(mvcResult).isNotNull();
+    }
+
+    @Test
+    public void taskFinderTaskPaged() throws Exception {
+        //given
+
+        PageRequest pageRequest = PageRequest.of(1,10);
+
+        TaskEntity taskEntity = buildDefaultTask();
+
+        given(taskRepository.findAll(any(), eq(pageRequest)))
+                .willReturn(new PageImpl<>(Collections.singletonList(taskEntity), pageRequest, 11));
+
+        //when
+        MvcResult mvcResult = mockMvc.perform(get("/abpm/{taskId}/new?page=1&size=10",
+                taskEntity.getId())
+                .accept(MediaTypes.HAL_JSON_VALUE))
+                //then
+                .andExpect(status().isOk())
+                .andReturn();
+
+        assertThat(mvcResult).isNotNull();
+    }
+
+    @Test
+    public void variableFinderTest() throws Exception {
+        //given
+        PageRequest pageRequest = PageRequest.of(1,10);
+
+        TaskVariableEntity variableEntity = buildVariable(1L);
+
+        given(variableRepository.findAll(any(), eq(pageRequest)))
+                .willReturn(new PageImpl<>(Collections.singletonList(variableEntity), pageRequest, 11));
+
+        //when
+        MvcResult mvcResult = mockMvc.perform(get("/abpm/{taskId}/variables?page=1&size=10",
+                variableEntity.getTaskId())
+                .accept(MediaTypes.HAL_JSON_VALUE))
+                //then
+                .andExpect(status().isOk())
+                .andReturn();
+
+        assertThat(mvcResult).isNotNull();
+
+    }
+
+    private TaskEntity buildDefaultTask() {
+        return new TaskEntity(UUID.randomUUID().toString(),
+                "john",
+                "Review",
+                "Review the report",
+                new Date(),
+                new Date(),
+                20,
+                UUID.randomUUID().toString(),
+                UUID.randomUUID().toString(),
+                "My app",
+                "My app",
+                "1",
+                null,
+                null,
+                Task.TaskStatus.ASSIGNED,
+                new Date(),
+                new Date(),
+                "peter",
+                null,
+                "aFormKey"
+        );
+    }
+
+    private TaskVariableEntity buildVariable(Long id) {
+        TaskVariableEntity variableEntity = new TaskVariableEntity(id,
+                String.class.getName(),
+                "firstName",
+                UUID.randomUUID().toString(),
+                "My app",
+                "My app",
+                "1",
+                null,
+                null,
+                UUID.randomUUID().toString(),
+                new Date(),
+                new Date(),
+                UUID.randomUUID().toString());
+        variableEntity.setValue("John");
+        return variableEntity;
+    }
 }
